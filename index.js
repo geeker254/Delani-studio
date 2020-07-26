2c5c7b8dc5c0ae12199dc9111cb8ea88daf01416
geeker254 <eugenebiwott@gmail.com>
2020-07-27 00:07:48 +0300

    Alert funtion

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 $('form#myForm').submit(function(){
     var name = $('#name').val();
     var pass = $('#email').val();
-    var mess = $('#mess').val();
-    alert("Hi " + name + " we have received your message and we will get in touch. Thank you for contacting us.");
+    var text = $('#mess').val();
+    alert("Hi! We have received your message and we will get in touch. Thank you for contacting us.");
   });
 
 $("button").on('click', function(){
