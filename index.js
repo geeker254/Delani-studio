9e874c5b99b87b1902fd5b64504bd2e319ad0a91
geeker254 <eugenebiwott@gmail.com>
2020-07-26 23:42:17 +0300

    Submit section

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-$(document).ready(function(){
-    $("form#form34A").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#MERGE1").val();
-      var email = $("input#MERGE0").val();
-      var message = $("textarea#comment").val();
-      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-    }
-});
-});
+
+$('form#myForm').submit(function(){
+    var name = $('#name').val();
+    var pass = $('#email').val();
+    var mess = $('#mess').val();
+    alert("Hi " + name + " we have received your message and we will get in touch. Thank you for contacting us.");
+  });
 
 $("button").on('click', function(){
     $('form').each(function(){
